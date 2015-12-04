b2ca6e3695ed1756a527b3426dd1fcf0c3274b1d
mmacedoeu <contato@mmacedo.eu.org>
2015-12-04 18:34:31 -0200

    read string

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use env_logger::LogBuilder;
 use std::fs::{File, OpenOptions};
 use std::sync::mpsc::channel;
 use std::io::{BufReader,BufRead};
+use std::io::Error;
 
 use miow::pipe::{NamedPipe, NamedPipeBuilder};
 use miow::iocp::CompletionPort;
@@ -30,11 +31,11 @@ extern fn interrupt(_:u32) {
 
 static mut stop_loop : Option<AtomicBool> = None;
 
-fn handle_pipe(name : &String) {
+fn handle_pipe(name : &String) -> Result<(), Error> {
 	let mut a = try!(NamedPipe::new(name));
-
+    let name2 = name.clone();
     let t = thread::spawn(move || {
-	    let mut f = File::create(name);
+	    let mut f = File::create(name2);
     });
 
     let cp = try!(CompletionPort::new(1));
@@ -43,15 +44,16 @@ fn handle_pipe(name : &String) {
     let mut b : String = String::new();
 	let mut over = Overlapped::zero();
     unsafe {
-    	a.read_overlapped(&mut b, &mut over);
+    	a.read_overlapped(&mut b.as_ref(), &mut over);
     }
     let status = cp.get(None);       
     info!("{:?}", b);
 
 	t.join();
+    Ok(())
 }
 
-fn name(symbol: &String, number: &String) -> String {
+fn name(symbol: String, number: String) -> String {
     format!(r"\\.\pipe\{}{}", symbol, number)
 }
 
@@ -73,12 +75,12 @@ fn main() {
 
     builder.init().unwrap();
 
-    let pipe_name = name(&param1, &param2);
+    let pipe_name = name(param1, param2);
     info!("Pipename: {:?}", &pipe_name);
 
     unsafe {
     	stop_loop = Some(AtomicBool::new(false));
       	signal(2, interrupt);
-    	handle_pipe(&pipe_name);      	
+    	handle_pipe(&pipe_name).unwrap();      	
     }
 }
